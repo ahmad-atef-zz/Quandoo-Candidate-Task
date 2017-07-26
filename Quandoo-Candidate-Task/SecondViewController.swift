2142b12c22b06494838a4f346b659d22dff7f617
Ahmad Atef <ahmad.3atef@gmail.com>
2017-07-27 00:08:59 +0200

    Project Organization.

---
+++ Quandoo-Candidate-Task/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Quandoo-Candidate-Task
+//
+//  Created by Ahmad Atef on 7/27/17.
+//  Copyright © 2017 Quandoo GmbH. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
